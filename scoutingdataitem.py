67b5b5175e496d3dd9e1f5247becfa6c349a6d3d
Boyne <54646090+DedFishy@users.noreply.github.com>
2024-02-29 10:26:28 -0500

    Worked on analyzer

--- scoutingdataitem.py
+++ scoutingdataitem.py
@@ -10,6 +10,12 @@ class ScoutingDataItem(customtkinter.CTkFrame):
         self.dataName = customtkinter.CTkLabel(self, text=str(team) + ": " + str(data_name), font=self.font)
         self.dataName.grid(row=0, column=0)
 
+        self.lineOfBestFit = customtkinter.CTkCheckBox(self, text="Line of Best Fit")
+        self.lineOfBestFit.grid(row=0, column=1)
+
+        self.calculateStdDeviation  = customtkinter.CTkCheckBox(self, text="Calculate Standard Deviation")
+        self.calculateStdDeviation.grid(row=0, column=2)
+
         self.data_name = data_name
         self.data_value_method = data_value_method
         self.team = team
